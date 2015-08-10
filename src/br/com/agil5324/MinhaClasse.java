9629aec400d7f17a5e5d6f2325e480d005bc1152
Jonathan <ramirezjlb@gmail.com>
2015-08-10 12:58:36 -0300

    Correcao BUG

---
+++ src/br/com/agil5324/MinhaClasse.java
@@ -0,0 +1,13 @@
+package br.com.agil5324;
+
+public class MinhaClasse {
+	int Numero;
+
+	public int getNumero() {
+		return Numero;
+	}
+
+	public void setNumero(int numero) {
+		Numero = numero;
+	}
+}
